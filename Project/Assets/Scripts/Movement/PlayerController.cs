97c0b418d256c4b39195b1cc00a1e2662bc7feb5
XSpirit <egor.tkach2003@gmail.com>
2021-01-05 06:09:51 +0300

    5.01.21
    EnemyController

---
+++ Project/Assets/Scripts/Movement/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float moveInput;
+    Movement move;
+    
+    void Start()
+    {    
+        move = GetComponent<Movement>();
+    }
+
+    
+    void Update()
+    {
+        
+        moveInput = Input.GetAxisRaw("Horizontal");
+        move.Move(moveInput);
+
+        if (Input.GetKey(KeyCode.Space))        
+        {
+            move.Jump();
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : Movement
+{
+    float health;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        if (health <= 0) Destroy(gameObject);
+    }
+}
