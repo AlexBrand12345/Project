036ba4fc997e309e9897593393f01c900af0127b
georgg2003 <egor.tkach2003@gmail.com>
2021-02-27 13:26:36 +0300

    Enemy's hand

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public sealed class Enemy : BaseEnemy
 {
+    [SerializeField] GameObject hand;
     // Start is called before the first frame update
     private new void Start()
     {
@@ -21,6 +22,20 @@ public sealed class Enemy : BaseEnemy
     private new void Update()
     {
         base.Update();
-        
+        //Hand Controller
+        if (SearchPlayer())
+        {
+            float x = -player.transform.position.x + transform.position.x;
+            float y = -player.transform.position.y + transform.position.y;
+            float gunAngle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            Debug.Log(gunAngle);
+            if (body.velocity.x <= -0.01f) hand.transform.rotation = Quaternion.Euler(0, -180, -gunAngle);
+            if (body.velocity.x >= 0.01f) hand.transform.rotation = Quaternion.Euler(0, 0, -gunAngle);
+        }
+        else
+        {
+            if (body.velocity.x <= -0.01f) hand.transform.rotation = Quaternion.Lerp(hand.transform.rotation,Quaternion.Euler(0, -180, 0), 0.1f);
+            if (body.velocity.x >= 0.01f) hand.transform.rotation = Quaternion.Lerp(hand.transform.rotation, Quaternion.Euler(0, 0, 0), 0.1f);
+        }
     }
 }
